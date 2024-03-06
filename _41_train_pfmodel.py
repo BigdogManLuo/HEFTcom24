0050e258967b3864ed6c17335b5d8b64014707db
BigdogManLuo <sashabanks@126.com>
2024-03-06 20:15:39 +0800

    updatae

--- _41_train_pfmodel.py
+++ _41_train_pfmodel.py
@@ -3,23 +3,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
-import lightgbm as lgb
+from utils import NNRegressor
 
 #加载数据
 modelling_table = pd.read_csv("data/dataset/price_modelling_table.csv")
 X=modelling_table.drop(columns=["SS_Price","DA_Price"])
 y=modelling_table[["SS_Price","DA_Price"]]
 
-#分为训练集和测试集
+#分为训练集、验证集和测试集
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.1, random_state=42)
 
-#训练模型（lightGBM）
-model = lgb.LGBMRegressor(n_estimators=1000)
-model.fit(X_train, y_train["SS_Price"])
 
-#测试
-y_pred = model.predict(X_test)
-print(mean_squared_error(y_test["SS_Price"], y_pred))
+#初始化模型  lrbatch_size,num_epochs,input_size,output_size,hidden_size,loss_fn,alpha,num_layers
+params = {
+   "lr": 0.01,
+    "batch_size": 256,
+    "num_epochs": 100,
+    "input_size": X_train.shape[1],
+    "output_size": y_train.shape[1],
+    "hidden_size": 64,
+    "loss_fn": "mse",
+}
+
+
 
 #可视化
 plt.scatter(y_test["SS_Price"], y_pred)
